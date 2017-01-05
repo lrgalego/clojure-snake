895f7005ea4a99676da9de3aac97ece002ebe56c
Lucas Galego <lrgalego@gmail.com>
2017-01-05 09:01:07 -0800

    remove core-async from keyboard event

--- src/snake/keyboard.cljs
+++ src/snake/keyboard.cljs
@@ -1,19 +1,9 @@
 (ns snake.keyboard
-  (:require-macros [cljs.core.async.macros :refer [go-loop]])
   (:require [reagent.core :as reagent :refer [atom]]
             [goog.dom :as dom]
-            [goog.events :as events]
-            [cljs.core.async :refer [put! chan <! >!]]))
-
-(defn event-activity [target event]
-  (let [event-broadcast (chan)]
-    (events/listen target event #(put! event-broadcast %))
-    event-broadcast))
+            [goog.events :as events]))
 
 (defn setup-keyboard [handle-keyboard]
   (let [body (dom/getElement "body")]
     (events/removeAll body "keydown")
-    (let [keyboard-activity (event-activity body "keydown")]
-      (go-loop []
-        (handle-keyboard (<! keyboard-activity))
-        (recur)))))
+    (events/listen body "keydown" #(handle-keyboard %))))
